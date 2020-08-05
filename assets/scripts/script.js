7c69c5610bfaacec89cc774877b53085cf50ba24
SuzanneNL <skr470@gmail.com>
2020-08-05 13:14:22 +0000

    Refactor by extracting nested conditions from the flipCard function, and turning them into functions on their own.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,15 +14,26 @@ function cardFlip(){
     hasFlippedCard = false;
     secondCard = this;
 
-    if (firstCard.dataset.number === secondCard.dataset.number) {
-        firstCard.removeEventListener('click', cardFlip);
-        secondCard.removeEventListener('click', cardFlip);
-    } else {
-        setTimeout(()=> {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-    }, 1500);
-}}}
+    matchCheck();
+    }
+}
 
+function matchCheck(){
+    if (firstCard.dataset.number === secondCard.dataset.number) {
+        freezeCards();
+    } else {
+        cardsFlipBack();
+}}
+
+function freezeCards(){
+    firstCard.removeEventListener('click', cardFlip);
+    secondCard.removeEventListener('click', cardFlip);
+}
+    
+function cardsFlipBack(){
+    setTimeout(()=> {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    }, 1500);}
 
 allMemoryCards.forEach(card => card.addEventListener('click', cardFlip)) 
