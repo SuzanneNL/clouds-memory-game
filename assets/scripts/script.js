55fbbdbf3d65adef42dd276c83a9ad1324bdd0fe
SuzanneNL <skr470@gmail.com>
2020-08-05 13:03:34 +0000

    Further write cardFlip function, to determine which card is flipped first and second. Set data attribute to the memory cards and log to the console to check if the data attribute is shown

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,21 @@
 const allMemoryCards = document.querySelectorAll('.memory-card');
+let firstCard, secondCard;
+let hasFlippedCard = false;
 
-function cardFlip() {
-    this.classList.toggle('flip');
-}
+
+
+function cardFlip(){
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    } else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    console.log(firstCard.dataset.number);
+    console.log(secondCard.dataset.number);
+}};
 
 allMemoryCards.forEach(card => card.addEventListener('click', cardFlip)) 
