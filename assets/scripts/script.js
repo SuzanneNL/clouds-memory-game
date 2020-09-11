2d176b32a0be014ced96c09e483431baeaaef1d9
SuzanneNL <skr470@gmail.com>
2020-09-11 14:15:12 +0000

    Minor change from class to id for the information modal close button

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -105,7 +105,7 @@ const toggleModal = () => {
 };
 
 document.querySelector('#show-modal').addEventListener('click', toggleModal);
-document.querySelector('.close-button').addEventListener('click', toggleModal);
+document.querySelector('#information-modal-close-button').addEventListener('click', toggleModal);
 
 /*When a user clicks on option 2, the second cloud image is replaced by a cloud name image*/
 function showNameCards() {
