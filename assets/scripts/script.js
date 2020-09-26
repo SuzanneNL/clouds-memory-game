ab05cfaf6c32252c100831169e72b65c9de5014c
SuzanneNL <skr470@gmail.com>
2020-09-26 14:51:03 +0000

    Further elaborate comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,7 @@
+/*This basis of this game was created with the help of a youtube tutorial (https://www.youtube.com/watch?v=ZniVgo8U7ek). 
+The game was then styled and changed to make it more interesting and interactive for the user*/
+
+/*Variables*/
 const allMemoryCards = document.querySelectorAll('.memory-card');
 const flipSound = new Audio("assets/audio/card-flip.flac");
 const successSound = new Audio("assets/audio/success.wav");
@@ -9,18 +13,26 @@ let lockBoard = false;
 let flippedCards;
 let countNum = document.querySelector('.counter span');
 let finalCount = document.querySelector('#final-count');
-/*This function shuffles the cards, making them switch positions. This function runs first."*/
+
+/*This function shuffles the cards, making them switch positions. This function is called first."*/
 function shuffle() {
 	allMemoryCards.forEach(card => {
 		let randomPosition = Math.floor(Math.random() * 16);
 		card.style.order = randomPosition;
 	});
 }
+
 shuffle();
-/*If the board is locked, or if the user clicks twice on the same card, this function is not executed.
-Clicked cards get the class of 'flip'. A sound is played when a card is clicked and flips, and the countUp function is run.
-The condition concerning hasFlippedCard checks it the card that's clicked is the first or second card.
-After the selection of the first and second card, the function matchCheck checks if they match.*/
+
+/*This event listener checks if a memory card is clicked and then runs the functions cardFlip and countUp.*/
+allMemoryCards.forEach(card => card.addEventListener('click', cardFlip, countUp));
+
+/*This function first checks if the board is locked. If so, it doesn't run.
+Then it checks if the user clicks twice on the same card. Tn that case this function is not executed.
+Clicked cards get the class of 'flip'. 
+A sound is played when a card is clicked and flips, and the countUp function is run.
+The condition concerning hasFlippedCard checks if the card that's clicked is the first or second card.
+After it has identified the first and second clicked cards, the function matchCheck is executed.*/
 function cardFlip() {
 	if(lockBoard) return;
 	if(this === firstCard) return;
@@ -37,13 +49,16 @@ function cardFlip() {
 		matchCheck();
 	}
 }
-/*This function counts the number of clicks. Because of its position in cardFlip, doubleclicking a card doesn't count. Only flips count.*/
+
+/*This function counts the number of clicks. Because of its position in cardFlip, doubleclicking a card doesn't count. Only flips count. 
+The number is shown in the click counter and in the win-modal*/
 function countUp() {
 	countNum.innerHTML++;
 	finalCount.innerHTML++;
 }
+
 /*This function checks if the first and second card match, by comparing the datasets. 
-If so, the cards are frozen, and a success sound plays. Also it runs a function to check if the game is completed.
+If so, the cards are frozen, and a success sound plays. They will not be flipped back. Also it runs a function to check if the game is completed.
 If there's no match, a function runs that makes the cards flip back.*/
 function matchCheck() {
 	if(firstCard.dataset.number === secondCard.dataset.number) {
@@ -56,6 +71,7 @@ function matchCheck() {
 		cardsFlipBack();
 	}
 }
+
 /*This function disables cards from flipping back by removing the eventlistener.
 It then runs a function that resets the board.*/
 function freezeCards() {
@@ -63,8 +79,9 @@ function freezeCards() {
 	secondCard.removeEventListener('click', cardFlip);
 	resetBoard();
 }
+
 /*This function makes the cards flip back (when there's no match), by removing the class of 'flip'.
-However, because of a time-out the board is locked, making it impossible for a user to flip a third card, 
+However, because of a time-out, the board is locked, making it impossible for a user to flip a third card, 
 until the first and second card have flipped back.*/
 function cardsFlipBack() {
 	lockBoard = true;
@@ -75,36 +92,42 @@ function cardsFlipBack() {
 		resetBoard();
 	}, 1500);
 }
+
 /*This function resets the values of the variables hasFlippedCard, lockBoard, firstCard and secondCard,
 so the user can continue playing.*/
 function resetBoard() {
 	[hasFlippedCard, lockBoard] = [false, false];
 	[firstCard, secondCard] = [null, null];
 }
+
 /*This function checks if the game is completed, by comparing the total number of memory cards with the number of flipped cards.
 When the game is completed, a win sound is played.
-Also, the class of visible is added to the win-modal, so a modal shows, saying "you win!"*/
+Also, the class of visible is added to the win-modal, so a modal shows, saying "you win! and showing the total number of clicks"*/
 function checkIfGameIsCompleted() {
 	if(flippedCards.length == 16) {
 		winSound.play();
 		document.querySelector('.win-modal').classList.add('visible');
 	}
 }
-/*This event listener checks if a card is flipped and then runs the functions cardFlip and countUp.*/
-allMemoryCards.forEach(card => card.addEventListener('click', cardFlip, countUp));
-/*This section toggles the class 'modal-hidden' to the div with a class of 'modal', to hide or show the modal.
+
+/*This arrow function toggles the class 'modal-hidden' to the div with a class of 'modal', to hide or show the Instructions modal.
 This occurst when (see the event listeners) the button Instructions or the close button in the modal are clicked.*/
 const toggleModal = () => {
 	document.querySelector('.modal').classList.toggle('modal-hidden');
 };
+
 document.querySelector('#show-modal').addEventListener('click', toggleModal);
 document.querySelector('.close-button').addEventListener('click', toggleModal);
-/*This section removes the class 'visible' from the win modal, so that the win modal can disappear when clicked on the close button*/
+
+/*This section removes the class 'visible' from the win modal, so that the win modal can disappear when a user on the close button*/
 const closeWinModal = () => {
 	document.querySelector('.win-modal').classList.remove('visible');
 };
+
 document.querySelector('#win-modal-close-button').addEventListener('click', closeWinModal);
-/*When a user clicks on option 2, the second cloud images are replaced by cloud name cards*/
+
+/*There are two ways to play this game. Either with images only (option 1, the default setting), or with images and names (option 2).
+A user can select option 2. In that case, the game is reset, and after a short time-out the second cloud images are replaced by cloud name cards*/
 function resetAndShowNameCards() {
 	resetGame();
 	setTimeout(() => {
@@ -112,6 +135,7 @@ function resetAndShowNameCards() {
 	}, 600);
 }
 
+/*This function replaces the second cloud images with the name cards, and adds a check mark to the second option*/
 function showNameCards() {
 	document.querySelector('.second-cloud-image1').classList.add('hide-card');
 	document.querySelector('.cloud-name1').classList.add('show-card');
@@ -133,8 +157,11 @@ function showNameCards() {
     document.querySelector('.option2').classList.remove('hide-check');
 }
 
+/*This is the event listener that checks if option2 is selected, and then runs the resetAndShowNameCards function*/
 document.querySelector('#option2').addEventListener("click", resetAndShowNameCards);
-/*When a user clicks on option 1, the cloud name cards are replaced by cloud images*/
+
+/*There are two ways to play this game. Either with images only (option 1, the default setting), or with images and names (option 2).
+A user can select option 1. In that case, the game is reset, and after a short time-out the name cards are replaced by cloud images*/
 function resetAndShowImageCards() {
 	resetGame();
 	setTimeout(() => {
@@ -142,6 +169,7 @@ function resetAndShowImageCards() {
 	}, 600);
 }
 
+/*This function replaces the name cards with the second image cards, and adds a check mark to the first option*/
 function showImageCards() {
 	document.querySelector('.second-cloud-image1').classList.remove('hide-card');
 	document.querySelector('.cloud-name1').classList.remove('show-card');
@@ -163,8 +191,11 @@ function showImageCards() {
     document.querySelector('.option2').classList.add('hide-check');
 }
 
+/*This is the event listener that checks if option1 is selected, and then runs the resetAndShowImageCards function*/
 document.querySelector('#option1').addEventListener("click", resetAndShowImageCards);
-/*Resetting the game*/
+
+/*This function plays a shuffle sound. It resets the game by first removing all flip classes. It then resets the counters to 0. 
+It adds the event listeners that were removed due to matches. It resets the board, and shuffles the cards after a short time-out*/
 function resetGame() {
     shuffleSound.play();
 	document.querySelector('.first-cloud-image1').classList.remove('flip');
@@ -199,4 +230,6 @@ function resetGame() {
 		shuffle();
 	}, 600);
 }
+
+/*When the button 'Play again' is clicked, the function resetGame runs*/
 document.querySelector('#play-again').addEventListener("click", resetGame);
